eb5cb33b0766f1293636b1af93f4e26e83a19347
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-27 19:24:42 -0500

    The name of the application is changed and the types of queries that we are going to handle in the project are added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portal--vacaciones--frontend';
+  title = 'Sistema gestion de vacaciones';
 }
