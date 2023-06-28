56b22c4cfb0c3a3721e098f7643fdf14c1005284
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-27 19:29:12 -0500

    the component is created to create the employees

---
+++ src/app/register-employees/register-employees.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-employees',
+  templateUrl: './register-employees.component.html',
+  styleUrls: ['./register-employees.component.css']
+})
+export class RegisterEmployeesComponent {
+
+}
