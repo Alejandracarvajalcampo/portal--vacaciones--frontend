8791ca49d3b5413a21350a998986c4de0170ec4e
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-27 19:37:29 -0500

    the methods to create an employee are obtained, the html and css for the app views are created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterEmployeesComponent } from './register-employees/register-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register-employees', component:RegisterEmployeesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/register-employees/register-employees.component.ts
+++ src/app/register-employees/register-employees.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-employees',
   templateUrl: './register-employees.component.html',
   styleUrls: ['./register-employees.component.css']
 })
-export class RegisterEmployeesComponent {
+export class RegisterEmployeesComponent implements OnInit{
+
+  employee :Employee = new Employee();
+
+  constructor(private employeeService:EmployeeService, private router:Router){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveEmployee(){
+    this.employeeService.registerEmployee(this.employee).subscribe(dato =>{
+      console.log(dato);
+      this.goToEmployeeList();
+    },error => console.log(error));
+  }
+
+  goToEmployeeList(){
+  this.router.navigate(['employees']);
+  }
+
+  onSubmit(){
+    this.saveEmployee();
+  }
 
 }
