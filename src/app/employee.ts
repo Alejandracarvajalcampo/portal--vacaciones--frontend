e93f70763e513ccdf9d3a796ad1319ed6abed5ad
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-27 19:12:37 -0500

    classes and services for employees and vacation request are created

---
+++ src/app/employee.ts
@@ -0,0 +1,16 @@
+export class Employee {
+  id: number;
+  document: number;
+  documentType: string;
+  names: string;
+  surnames: string;
+  phone: string;
+  charge: string;
+  address: string;
+  admissionDate: Date;
+  withdrawalDate: Date;
+  typeContract: string;
+  state: string;
+  supervisor: string;
+
+}
---
+++ src/app/vacation-request.ts
@@ -0,0 +1,12 @@
+export class VacationRequest {
+  id: number;
+  document: number;
+  requestType: string;
+  startDate: Date;
+  endDate: Date;
+  days:number;
+  date:Date;
+  comment:string;
+  hireDate: Date;
+  state: string;
+}
