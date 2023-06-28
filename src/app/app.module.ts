f4fe04e1c6538f8ca77d9c03d35b26a4e30ed154
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-27 19:39:57 -0500

    component is created to update the employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterEmployeesComponent } from './register-employees/register-employees.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterEmployeesComponent
+    RegisterEmployeesComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
