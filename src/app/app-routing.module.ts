cb405194b60291c355a1b43f2a37605d0aa0a30b
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-30 10:20:14 -0500

    components for html views are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
 import { RegisterEmployeesComponent } from './register-employees/register-employees.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { DetailsEmployeeComponent } from './details-employee/details-employee.component';
+import { VacationRequestComponent } from './vacation-request/vacation-request.component';
+import { ListRequestComponent } from './list-request/list-request.component';
+
 
 const routes: Routes = [
+  {path: 'vacationRequests', component:ListRequestComponent},
+  {path: '',redirectTo:'vacationRequests',pathMatch:'full'},
+  {path: 'employees', component:ListEmployeeComponent},
+  {path: '',redirectTo:'employees',pathMatch:'full'},
   {path: 'register-employees', component:RegisterEmployeesComponent},
+  {path: 'update-employee/:id', component:UpdateEmployeeComponent},
+  {path: 'details-employee/:document', component:DetailsEmployeeComponent},
+  {path: 'vacation-request', component:VacationRequestComponent},
+
+
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterEmployeesComponent } from './register-employees/register-employees.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+import { DetailsEmployeeComponent } from './details-employee/details-employee.component';
+import { ListRequestComponent } from './list-request/list-request.component';
+import { VacationRequestComponent } from './vacation-request/vacation-request.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterEmployeesComponent,
-    UpdateEmployeeComponent
+    UpdateEmployeeComponent,
+    ListEmployeeComponent,
+    DetailsEmployeeComponent,
+    ListRequestComponent,
+    VacationRequestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
